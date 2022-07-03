099a87a15b86cbfd82fcc1b4b1f058d71defe6a9
lvlHindu <107579434+lvlHindu@users.noreply.github.com>
2022-07-04 02:06:05 +0300

    Add files via upload

---
+++ 22.06.2022/Day6Zadanie2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> list = new List<int>();
+                Random random = new Random();
+                int CountClients = random.Next(1, 11);
+                int money = 0;
+                for (int i = 0; i < CountClients; i++)
+                {
+                    list.Add(random.Next(150, 45000));
+                }
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.Clear();
+                    money += list[i];
+                    Console.WriteLine("Клиент заплатил: " + list[i] + " $");
+                    Console.WriteLine("Ваши деньги: " + money + " $");
+                    Console.WriteLine("Нажмите любую клавишу(Кроме кнопки выключения ПК) чтобы продолжить......");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+                Console.WriteLine("Клиентов больше нет");
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ 22.06.2022/Day6Zadanie3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthArray = 0;
+            int[] array = new int[lengthArray];
+            int[] arrayCopy = new int[lengthArray];
+            int arraySum = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Введите число для запоминания, exit или sum: ");
+                string Message = Console.ReadLine();
+
+                if (Message != "sum" && Message != "exit")
+                {
+                    int nomberRand = Convert.ToInt32(Message);
+                    lengthArray += 1;
+                    array = new int[lengthArray];
+
+                    for (int i = 0; i < arrayCopy.Length; i++)
+                    {
+                        array[i] = arrayCopy[i];
+                    }
+
+                    array[lengthArray - 1] = nomberRand;
+                    arrayCopy = new int[lengthArray];
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        arrayCopy[i] = array[i];
+                    }
+
+                    arrayCopy[lengthArray - 1] = nomberRand;
+                }
+
+                else if (Message == "sum")
+                {
+                    for (int i = 0; i < array.Length; i++)
+                        arraySum += array[i];
+                    Console.WriteLine($"Сумма массива: {arraySum} ");
+                    arraySum = 0;
+                }
+
+
+                else if (Message == "exit")
+                {
+                    break;
+                }
+
+                else Console.WriteLine($"Данная команда отсутствует ");
+            }
+        }
+    }
+}
